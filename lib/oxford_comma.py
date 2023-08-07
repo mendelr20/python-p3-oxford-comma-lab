8e3dd705319c6bfb51f020f06e8dfc139d08ef8d
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-08-07 17:43:54 -0400

    Complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        str = ''.join(items)
+    elif len(items) == 2:
+        str = " and ".join(items)
+    else:
+        str = f"{', '.join(items[:-1])}, and {items[-1]}"
+        
+    return str
+        
